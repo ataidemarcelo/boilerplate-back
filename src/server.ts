3f5706a7bde3abf203e9f54b9b12bdd6a8caed3f
Marcelo Ataíde <msa.trybe@gmail.com>
2023-10-04 10:54:48 -0300

    feat: add environment variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
+import 'dotenv/config';
 import App from './App';
 
-const PORT = (process.env.APP_PORT != null) || 3001;
+const PORT = process.env.API_PORT ?? 3001;
 
 new App().start(Number(PORT));
