1482af8fd3d526e82734cb8753e45c0f3f52af16
Marcelo Ataíde <102421810+ataidemarcelo@users.noreply.github.com>
2023-10-09 11:40:56 -0300

    feat/add-sequelize (#13)
    * fix: move morgan's call to the top #7 
    
    * chore: install dependencies #7
    
    * chore: create  file #7
    
    * fix: remove command git add #7
    
    * feat: create docker-compose file #7
    
    * feat: create and configure db-api database #7
    
    * fix: adjust coverage #7

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import AppInstance from './App';
 
+// import './database/models'; // Ccarrega arquivo e executa o testConnection em database/models/index.ts
+
 const PORT = process.env.API_PORT ?? 3001;
 
 AppInstance.app.listen(Number(PORT), () => { console.log(`API running at port ${PORT}`); });
