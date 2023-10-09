2e228d3fcf07b6ea4631956864ac6101f72cdbe6
Marcelo Ataíde <msa.trybe@gmail.com>
2023-10-09 11:30:06 -0300

    fix: adjust coverage

--- jest.config.ts
+++ jest.config.ts
@@ -24,7 +24,8 @@ const config: Config = {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
     '<rootDir>/src/**/*.ts',
-    '!<rootDir>/src/server.ts'
+    '!<rootDir>/src/server.ts',
+    '!<rootDir>/src/database/**'
   ],
 
   // The directory where Jest should output its coverage files
