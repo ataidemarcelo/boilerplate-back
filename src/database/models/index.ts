dd4f1270430359b364099d195abd11125a62637c
Marcelo Ataíde <msa.trybe@gmail.com>
2023-12-11 10:16:19 -0300

    test: adjusts the bank connection test to run only in development

--- src/database/models/index.ts
+++ src/database/models/index.ts
@@ -3,17 +3,23 @@ import * as config from '../config/database';
 
 const sequelize = new Sequelize(config);
 
-const testConnection = async (): Promise<any> => {
+const testConnection = async (): Promise<void> => {
   try {
     await sequelize.authenticate();
-    // console.log('Connection has been established successfully!!!');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
+    throw error;
   }
 };
 
-testConnection()
-  .then((_ret) => { console.log('Connection has been established successfully.'); })
-  .catch((err) => { console.log(err); });
+if (process.env.NODE_ENV === 'development') {
+  testConnection()
+    .then(() => {
+      console.log('Connection has been established successfully.');
+    })
+    .catch((err) => {
+      console.error('Error during database connection.', err);
+    });
+}
 
 export default sequelize;
